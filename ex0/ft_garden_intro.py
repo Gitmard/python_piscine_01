c3ac488fb97cd69edacd4c04cfe3ae9b61ad775c
smenard <floogles71@gmail.com>
2026-02-04 17:31:34 +0100

    feat: ex0

---
+++ ex0/ft_garden_intro.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def main():
+    print("=== Welcome to My Garden ===")
+    name = "Iris"
+    height = "23cm"
+    age = "34 days"
+    print(f"Plant: {name}")
+    print(f"Height: {height}")
+    print(f"Age: {age}")
+    print("\n=== End of Program ===")
+
+
+if __name__ == "__main__":
+    main()
